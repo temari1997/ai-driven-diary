b9d40c0bb9433c0ca32e6fddf44ea0745a0d8eb5
temari(hadaly) <kurakore112@gmail.com>
2025-08-17 09:32:08 +0900

    fix(nginx): Update PORT placeholder in nginx configuration and enhance debug output in startup script

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-# Replace the placeholder with the PORT environment variable
-envsubst < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+# Replace the '${PORT}' placeholder with the PORT environment variable
+envsubst '${PORT}' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
+
+# Output the generated config file for debugging
+echo "--- Generated nginx.conf ---"
+cat /etc/nginx/conf.d/default.conf
+echo "--------------------------"
 
 # Start Nginx
 nginx -g 'daemon off;'
